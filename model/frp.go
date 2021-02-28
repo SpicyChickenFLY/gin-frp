6ce41ee25f7de1ff4111a38d018dc1a1d4777e21
SpicyChickenFLY <xixiayuchao@gmail.com>
2021-02-28 23:22:23 +0800

    add inintalize func for struct

--- model/frp.go
+++ model/frp.go
@@ -1,38 +1,80 @@
 package model
 
+// ServerInfo is a frp server info struct
 type ServerInfo struct {
 	address string
 	port    int
 	token   string
 }
 
+// FrpCommonService is a general frp service struct
 type FrpCommonService struct {
 	Name string
 	Type string
 }
 
-type FrpTcpService struct {
+// FrpTCPService is a tcp type frp service struct
+type FrpTCPService struct {
 	FrpCommonService
 	LocalIP   string
 	LocalPort int
 	SecretKey string
 }
 
+// NewFrpTCPService initialize new FrpTCPService struct
+func NewFrpTCPService(
+	name, serviceType string,
+	localIP string, localPort int, secretKey string) *FrpTCPService {
+	return &FrpTCPService{
+		FrpCommonService: FrpCommonService{
+			Name: name,
+			Type: serviceType,
+		},
+		LocalIP:   localIP,
+		LocalPort: localPort,
+		SecretKey: secretKey,
+	}
+}
+
+// FrpFilePlugin is a file frp plugin struct
 type FrpFilePlugin struct {
 	Name         string
 	LocalPath    string
 	StripPrefix  string
-	HttpUser     string
-	HttpPassword string
+	HTTPUser     string
+	HTTPPassword string
 }
 
+// FrpFileService is a file type frp service struct
 type FrpFileService struct {
 	FrpCommonService
 	FrpFilePlugin
 	RemotePort int
 }
 
-type FrpHttpsPlugin struct {
+// NewFrpFileService initialize new FrpFileService struct
+func NewFrpFileService(
+	serviceName, serviceType string,
+	pluginName, pluginPath, pluginPrefix, pluginUser, pluginPwd string,
+	remotePort int) *FrpFileService {
+	return &FrpFileService{
+		FrpCommonService: FrpCommonService{
+			Name: serviceName,
+			Type: serviceType,
+		},
+		FrpFilePlugin: FrpFilePlugin{
+			Name:         pluginName,
+			LocalPath:    pluginPath,
+			StripPrefix:  pluginPrefix,
+			HTTPUser:     pluginUser,
+			HTTPPassword: pluginPwd,
+		},
+		RemotePort: remotePort,
+	}
+}
+
+// FrpHTTPSPlugin is a https frp plugin struct
+type FrpHTTPSPlugin struct {
 	Name              string
 	LocalAddress      string
 	CertificatePath   string
@@ -41,7 +83,30 @@ type FrpHttpsPlugin struct {
 	HeaderXFromWhere  string
 }
 
-type FrpHttpsService struct {
+// FrpHTTPSService is a https type frp service struct
+type FrpHTTPSService struct {
 	FrpCommonService
-	FrpHttpsPlugin
+	FrpHTTPSPlugin
+}
+
+// NewFrpHTTPSService initialize new FrpHTTPSService struct
+func NewFrpHTTPSService(
+	serviceName, serviceType string,
+	pluginName, pluginAddr, pluginCrt, pluginKey,
+	pluginRewrite, pluginWhere string,
+	remotePort int) *FrpHTTPSService {
+	return &FrpHTTPSService{
+		FrpCommonService: FrpCommonService{
+			Name: serviceName,
+			Type: serviceType,
+		},
+		FrpHTTPSPlugin: FrpHTTPSPlugin{
+			Name:              pluginName,
+			LocalAddress:      pluginAddr,
+			CertificatePath:   pluginCrt,
+			KeyPath:           pluginKey,
+			HostHeaderRewrite: pluginRewrite,
+			HeaderXFromWhere:  pluginWhere,
+		},
+	}
 }
